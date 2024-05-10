ee3d17068b8a9bcf9ad50aca6553c74a615a16e6
Shubham Sapra <shubham.sapra97@gmail.com>
2024-05-10 07:15:00 +0530

    Update constants.js

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -3,6 +3,6 @@ module.exports = {
     TOKEN_SECRET: "$snarki#001#Restaurants$",
     sendgrid_from_email: "ssapra@snarkiapp.com",
     sendgrid_contact_to_email: "support@snarkiapp.com",
-    sendgrid_contact_template_id: "d-5a34e0c379044730bc98283679636273",
-    sendgrid_reset_password_template_id: "d-187bc69d144445429bd549d7984bc40a"
+    sendgrid_contact_template_id: "",
+    sendgrid_reset_password_template_id: ""
 };
